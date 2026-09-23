c0e92b7aa1bd53542d63854e6bab6d269404d247
Preet Patel <preetpatel.genai@gmail.com>
2026-09-23 09:36:04 +0530

    Time: 263 ms (49.19%), Space: 96.3 MB (19.47%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,43 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const cache = new Map();
+    let callCount = 0;
+
+    const memoized = function(...args) {
+        const key = JSON.stringify(args);
+
+        // Already calculated
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+
+        // First time seeing these arguments
+        const result = fn(...args);
+
+        callCount++;
+        cache.set(key, result);
+
+        return result;
+    };
+
+    memoized.getCallCount = function() {
+        return callCount;
+    };
+
+    return memoized;
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
